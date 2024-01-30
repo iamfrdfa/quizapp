7096c8466e8d8c9cbe4c6bfedfc380bdaa48ec62
iamfrdfa <fri.faraji@gmail.com>
2024-01-30 21:37:18 +0100

    inserted new html quiz-structure and some stylings

---
+++ script.js
@@ -0,0 +1,39 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Justin Bieber",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Lady Gaga",
+        "right_answer": 3
+    },
+    {
+        "question": "Welches Attribut kann man NICHT für Textarea verwenden?",
+        "answer_1": "readonly",
+        "answer_2": "max",
+        "answer_3": "from",
+        "answer_4": "spellcheck",
+        "right_answer": 1
+    },
+    {
+        "question": "Wie wählst du alle Elemente vom Typ &lt;a?gt; mit dem Attribut title aus?",
+        "answer_1": "a[title]{...}",
+        "answer_2": "a > title {...}",
+        "answer_3": "a.title {...}",
+        "answer_4": "a=title {...}",
+        "right_answer": 1
+    },
+    {
+        "question": "Wie definiert man in Javascript eine Variable?",
+        "answer_1": "let 100 = rate;",
+        "answer_2": "100 = let rate;",
+        "answer_3": "rate = 100;",
+        "answer_4": "let rate = 100;",
+        "right_answer": 3
+    },
+    
+];
+
+function init() {
+
+}
