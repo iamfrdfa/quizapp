42050c7bcbc6503ad722dc5b0a0892b29a190ebf
iamfrdfa <fri.faraji@gmail.com>
2024-01-31 13:32:21 +0100

    selectAnswer function and stylings

--- script.js
+++ script.js
@@ -34,6 +34,34 @@ let questions = [
     
 ];
 
-function init() {
+let currentQuestion = 0;
 
+function showQuestion() {
+    let question = questions[currentQuestion];
+    
+    document.getElementById('questionText').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    console.log('Selected answer is ', selection);
+    let selectedQuestionNumber = selection.slice(-1);
+    console.log('selectedQuestionNumber is ', selectedQuestionNumber);
+    console.log('Current question is ', question['right_answer']);
+    
+    if (selectedQuestionNumber === question['right_answer']) {
+        console.log('Richtige Antwort!')
+    }
+    else {
+        console.log('Falsche Antwort!')
+    }
+}
+
+function init() {
+    document.getElementById('all-questions').innerHTML = questions.length;
+    showQuestion();
 }
