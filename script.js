ac05c7cf3fcb23b769e9fef199cb7afb4e18dd77
iamfrdfa <fri.faraji@gmail.com>
2024-02-03 11:16:01 +0100

    mainly done there. Next step is an exit screen

--- script.js
+++ script.js
@@ -37,19 +37,28 @@ let questions = [
 let currentQuestion = 0;
 
 function showQuestion() {
-    let question = questions[currentQuestion];
-    
-    document.getElementById('questionText').innerHTML = question['question'];
-    document.getElementById('answer_1').innerHTML = question['answer_1'];
-    document.getElementById('answer_2').innerHTML = question['answer_2'];
-    document.getElementById('answer_3').innerHTML = question['answer_3'];
-    document.getElementById('answer_4').innerHTML = question['answer_4'];
+    document.getElementById('question-number').innerHTML = currentQuestion + 1;
+    if (currentQuestion >= questions.length) {
+        document.getElementById('endScreen').style = ``;
+        document.getElementById('question-body').style = `display: none;`;
+    }
+    else {
+        let question = questions[currentQuestion];
+        
+        document.getElementById('questionText').innerHTML = question['question'];
+        document.getElementById('answer_1').innerHTML = question['answer_1'];
+        document.getElementById('answer_2').innerHTML = question['answer_2'];
+        document.getElementById('answer_3').innerHTML = question['answer_3'];
+        document.getElementById('answer_4').innerHTML = question['answer_4'];
+    }
 }
 
 function answer(selection) {
     let question = questions[currentQuestion];
     console.log('Selected answer is', selection);
     let selectedQuestionNumber = selection.slice(-1);
+    console.log('selectedQuestionNumber is ', selectedQuestionNumber);
+    console.log('Current question is ', question['right_answer']);
     
     let idOfRightAnswer = `answer_${question['right_answer']}`;
     
@@ -65,7 +74,21 @@ function answer(selection) {
 
 function nextQuestion() {
     currentQuestion++; //oben wird currentQuestion mit Null definiert (0-te stelle der Fragen, also Real 1. Frage). Für die nächste Frage müssen wir um eins erhöhen
-    showQuestion();       
+    showQuestion();
+
+    document.getElementById('next-btn').disabled = false;
+    resetAnswerButtons();
+}
+
+function resetAnswerButtons() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
 }
 
 function init() {
